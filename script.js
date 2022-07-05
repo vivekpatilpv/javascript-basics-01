cbd427aa94adc10677d45dfd8956e5542218d047
vivekpatilpv <vivekpatil.5678@gmail.com>
2022-07-06 02:53:39 +0530

    css fixed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function Submit() {
-    var sideA;
-    var sideB;
-    var sideC;
-    sideA = document.getElementById("sideA").value;
-    sideB = document.getElementById("sideB").value;
-    sideC = Math.pow(sideA, 2) + Math.pow(sideB, 2);
-    sideC = Math.sqrt(sideC);
-    sideC = parseInt(Math.round(sideC));
-    document.getElementById("sideC").innerHTML = sideC;
-  }
-  
+const side = document.querySelectorAll(".sides");
+const button = document.querySelector(".button");
+const result = document.querySelector("#result");
+
+const hypo = () => {
+    if (side[0].value > 0 && side[1].value > 0){
+        const hypotenuse = (Math.hypot(side[0].value, side[1].value));;
+        result.innerText = "Hypotenuse is :- " + hypotenuse ;
+    }else{
+        result.innerText = "Oops! Enter positive values.";
+    }
+};
+
+button.addEventListener("click",hypo);
